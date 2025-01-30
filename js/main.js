edb3e4b696444fd1279193458f286c09b23f5c43
MaryaLauraRS <131805559+Rioske-Nakamura@users.noreply.github.com>
2025-01-30 18:38:39 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,7 +76,76 @@ function updatePhotoGallery() {
     gallery.appendChild(iframe);
   });
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const reg = await navigator.serviceWorker.register('.../sw.js', { type: "module" });
+      console.log('Service Worker registrado!', reg);
+      postNews();
+    } catch (err) {
+      console.log('Registro do Service Worker falhou:', err);
+    }
+  });
+}
 
+const photosData = [
+  {
+    latitude: -22.214346824182048, 
+    longitude: -53.33518339065308,
+    imagem: "https://i.pinimg.com/736x/11/aa/23/11aa23c22cd0752a71b7ee1a32f81fcc.jpg"
+  },
+  {
+    latitude: -22.569880096410415, 
+    longitude: -53.064891951778144,
+    imagem: "https://i.pinimg.com/474x/d1/be/7f/d1be7f6664600b70a5716cb6c93aa450.jpg"
+  },
+  {
+    latitude: -22.096453872803153, 
+    longitude: -53.44228944072136,
+    imagem: "https://i.pinimg.com/736x/fa/29/4f/fa294f53812229a9443635669afa6c70.jpg"
+  },
+  {
+    latitude: -22.229431858164613, 
+    longitude: -53.33654964587059,
+    imagem: "https://i.pinimg.com/736x/9d/d1/d4/9dd1d4b9ee4fef428c7ff5c65df9fc72.jpg"
+  },
+  {
+    latitude: -22.343058134790585, 
+    longitude: -53.71800579135069,
+    imagem: "https://i.pinimg.com/474x/21/0c/0c/210c0c54bb5cc94461b629ae8e944e7e.jpg"
+  },
+  {
+    latitude: -22.211138262011172, 
+    longitude: -53.33336583728294,
+    imagem: "https://i.pinimg.com/736x/b6/da/20/b6da20d9218f8130062fc15f28d93d76.jpg"
+  }
+];
+
+const cardsContainer = document.getElementById("cards-container");
+const photoMap = document.getElementById("photo-map");
+
+function createPhotoCards() {
+  photosData.forEach((photo) => {
+    const card = document.createElement("div");
+    card.classList.add("photo-card");
+
+    const img = document.createElement("img");
+    img.src = photo.imagem;
+    img.alt = "Foto com localização";
+
+    card.appendChild(img);
+    cardsContainer.appendChild(card);
+
+    // Ao clicar no card, atualiza o iframe do mapa
+    card.addEventListener("click", () => {
+      const mapUrl = `https://www.google.com/maps?q=${photo.latitude},${photo.longitude}&z=15&output=embed`;
+      photoMap.src = mapUrl;
+    });
+  });
+}
+
+// Chamando a função para exibir os cards na tela
+createPhotoCards();
 function cameraStart() {
   navigator.mediaDevices
     .getUserMedia(constraints())
